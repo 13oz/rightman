1f65d1f5805b6e497ca2a348b216cea81697374a
13oz <duminsky.nick@gmail.com>
2014-03-13 23:59:10 +0400

    The models became one. Created some methods interfaces

--- models.py
+++ models.py
@@ -7,63 +7,50 @@ from django.contrib.contenttypes.models import ContentType
 class Company(models.Model):
     companyName = models.CharField(max_length = 120)
 
+class UserAccount(models.Model):
+    #user roles
+    user_roles  = (
+        ("admin", "site administrator account"),
+        ("power user", "company administrator account"),
+        ("user", "simple user account"))
+
+    user_object = models.ForeignKey(ContentType)
+    user_id = models.PositiveIntegerField()
+    user = generic.GenericForeignKey('user_object', 'user_id')
+    user_role = models.CharField(max_length = 1, choices = user_roles)
+    user_company = models.OneToOneField(Company, related_name = 'employee_of')
+
+    def is_root(self, acc_id):
+        return True
+
+    def is_power(self, acc_id):
+        return True
+
+    def employee_of(self, acc_id):
+        return True
+
 class Permissions(models.Model):
+    #Groups: ANY, COMPANY, OWNER
+    #Actions: READ, MODIFY
+    read_choise = (
+        ("ANY", "Anyone can see this object"),
+        ("COMPANY", "This object can be seen only by owner's company employees"),
+        ("OWNER", "This object can be seen only by it's owner"))
+
+    modify_choise = (
+        ("ANY", "Anyone can modify this object"),
+        ("COMPANY", "This object can be modfied only be owner's company employees"),
+        ("OWNER", "This object can be modified only by it's owner"))
+
     account = models.ForeignKey(ContentType)
     user_id = models.PositiveIntegerField()
-    owner = generic.GenericForeignKey('account', 'user_id')              
+    owner = models.OneToOneField(UserAccount, related_name = 'owner')
     company = models.OneToOneField(Company, related_name = 'property_of')
     ruledObject = generic.GenericForeignKey('content_type', 'object_id')
     #TODO permission bits
 
     #return row owner
     def get_owner(self, row_id):
-        return User.objects.filter(id=OwnerMixin.objects.filter(id=row_id).owner)
+        return self.objects.filter(id=row_id)[0].owner
 
     #TODO permission checks
-
-
-class PowerUsers(models.Model):
-    powerUser = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    user = generic.GenericForeignKey('powerUser', 'object_id')              
-    company = models.OneToOneField(Company, related_name = 'company_power_user')
-
-    def save(self, *args, **kwargs):
-        for elem in PlainUsers.objects.all():
-            if self.owner == elem.owner:
-                elem.delete()
-        for elem in Admins.objects.all():
-            if self.owner == elem.owner:
-                elem.delete()
-        super(PowerUsers, self).save(*args, **kwargs)
-
-
-class PlainUsers(models.Model):
-    plainUser = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    user = generic.GenericForeignKey('plainUser', 'object_id')              
-    company = models.OneToOneField(Company, related_name = 'company_plain_user')
-
-    def save(self, *args, **kwargs):
-        for elem in PowerUsers.objects.all():
-            if self.owner == elem.owner:
-                elem.delete()
-        for elem in Admins.objects.all():
-            if self.owner == elem.owner:
-                elem.delete()
-        super(PlainUsers, self).save(*args, **kwargs)
-
-class Admins(models.Model):
-    plainUser = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    user = generic.GenericForeignKey('plainUser', 'object_id')              
-    company = models.OneToOneField(Company, related_name = 'company_plain_user')
-
-    def save(self, *args, **kwargs):
-        for elem in PlainUsers.objects.all():
-            if self.owner == elem.owner:
-                elem.delete()
-        for elem in Admins.objects.all():
-            if self.owner == elem.owner:
-                elem.delete()
-        super(PowerUsers, self).save(*args, **kwargs)
